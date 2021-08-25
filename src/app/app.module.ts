0e76f28bda792ca9f8e249819870d487e219597f
dmiloloza <domagoj.miloloza@gmail.com>
2021-08-25 11:23:27 +0200

    revert to master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SuccessAlertComponent } from './componetns/success-alert/success-alert.component';
-import { WarningAlertComponent } from './components/warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SuccessAlertComponent,
-    WarningAlertComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
