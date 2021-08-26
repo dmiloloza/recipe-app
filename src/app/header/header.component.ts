e17f3aced0b970fe1836f7d411e94013ffed45bf
dmiloloza <domagoj.miloloza@gmail.com>
2021-08-26 12:20:28 +0200

    header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  collapsed: boolean = true;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
