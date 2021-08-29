d834c6e99d2436eefa3169ba282cdcf32380bbb1
dmiloloza <domagoj.miloloza@gmail.com>
2021-08-29 13:06:52 +0200

    Add new items to shopping list close #9

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from '../../shared/ingredients.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', {static: true}) name!: ElementRef;
+  @ViewChild('amountInput', {static: true}) amount!: ElementRef;
+
+  @Output() newIngredient = new EventEmitter<Ingredient>();
 
   constructor() {
   }
@@ -13,4 +18,19 @@ export class ShoppingEditComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onAdd() {
+    if (!this.name.nativeElement.value || !this.amount.nativeElement.value) {
+      return alert('Please add Name and Amount of Ingredient.');
+    }
+    const ingredient = new Ingredient(this.name.nativeElement.value, this.amount.nativeElement.value);
+    this.newIngredient.emit(ingredient);
+  }
+
+  onDelete() {
+
+  }
+
+  onClear() {
+
+  }
 }
