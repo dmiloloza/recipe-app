d62f1a80a2d552e3d5ad09d2692e5d18390f94d3
dmiloloza <domagoj.miloloza@gmail.com>
2021-09-01 15:54:16 +0200

    dropdown directive adds open class to elements close #15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  link: string = 'shopping-list';
+  link: string = 'recipe';
 
   onNavigate(parameter: string) {
     this.link = parameter;
